31ec74fb8146e6ee8fe1a0ca9f5f6cd21d0888cb
Git-EDO <DemonEX@bk.ru>
2022-07-05 09:57:27 +0300

    menu fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 let body = document.querySelector('body');
+let burgerOpen = document.querySelector('.burger-open');
+let burgerClose = document.querySelector('.burger-close');
 let menu = document.querySelector('.menu__list');
 
-body.onclick = function() {
-    body.classList.toggle('lock');
+burgerOpen.onclick = function() {
+    body.classList.add('lock');
 }
 
+burgerClose.onclick = function() {
+    body.classList.remove('lock');
+}
 
+menu.onclick = function() {
+    body.classList.remove('lock');
+}
